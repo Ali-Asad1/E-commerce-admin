26836dbf5501b567eb59d01894a0d85f9c6e27bc
Ali Asad <aliasad.3128@gmail.com>
2024-01-20 11:10:25 +0330

    Add `root` route group
    add this route group for main layout

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+const RootPage = () => {
+  return (
+    <div>
+      <h1>welcome to home page</h1>
+    </div>
+  );
+};
+export default RootPage;
