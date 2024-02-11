2353bdb36769aba04091244749dc90e2ddfd36f2
Ali Asad <aliasad.3128@gmail.com>
2024-02-11 23:30:56 +0330

    feat: Update store creation `API` to check for existing store names

--- src/app/api/store/route.ts
+++ src/app/api/store/route.ts
@@ -6,22 +6,27 @@ import prismadb from "@/lib/prismadb";
 export async function POST(req: NextRequest) {
   try {
     const { userId } = auth();
-    const body = await req.json();
-    const { name } = body;
+    const { name } = await req.json();
 
     if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
     if (!name) return new NextResponse("Name is required!", { status: 400 });
 
-    const store = await prismadb.store.create({
-      data: {
-        name,
-        userId,
-      },
+    const existingStore = await prismadb.store.findFirst({
+      where: { name },
     });
 
-    return NextResponse.json(store);
+    if (existingStore) {
+      return NextResponse.json({ error: { name: "Store name already exists" } }, { status: 400 });
+    }
+
+    const newStore = await prismadb.store.create({
+      data: { name, userId },
+    });
+
+    return NextResponse.json(newStore);
   } catch (err: unknown) {
-    console.log(err);
+    console.error(err);
     return new NextResponse("Internal error!", { status: 500 });
   }
 }
