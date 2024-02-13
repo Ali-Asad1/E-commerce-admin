4fdf337241d03d856fa9a62043144edf43dc2aef
Ali Asad <aliasad.3128@gmail.com>
2024-02-13 21:49:15 +0330

    refactor: Improve structure and export types in `formUtils`

--- src/utils/fromUtils.ts
+++ src/utils/fromUtils.ts
@@ -1,8 +1,8 @@
 import { UseFormSetError, ErrorOption } from "react-hook-form";
 
-type FormErrorType = { name: string } & ErrorOption;
-type FormErrorListType = FormErrorType[];
-type SetErrorsType = (setError: UseFormSetError<any>, errors: FormErrorListType) => void;
+export type FormErrorType = { name: string } & ErrorOption;
+export type FormErrorListType = FormErrorType[];
+export type SetErrorsType = (setError: UseFormSetError<any>, errors: FormErrorListType) => void;
 
 export const setErrorsForInputs: SetErrorsType = (setError, errors) => {
   errors.forEach(({ name, type, message }) => {
