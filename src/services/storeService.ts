c4f36c74122c9fd3705abc9da03dd3c484d606bb
Ali Asad <aliasad.3128@gmail.com>
2024-02-13 21:48:50 +0330

    refactor: Updated `storeService` to handle form errors

--- src/services/storeService.ts
+++ src/services/storeService.ts
@@ -2,18 +2,24 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import toast from "react-hot-toast";
 
 import { StoreType } from "@/types/Store.type";
+import { FormErrorListType } from "@/utils/fromUtils";
 
-export const createStore = async (name: string): Promise<StoreType | AxiosError> => {
+export const createStore = async (
+  name: string
+): Promise<StoreType | AxiosError<FormErrorListType>> => {
   const loadingToastId = toast.loading("Loading...");
   try {
-    const response: AxiosResponse<StoreType> = await axios.post("/api/store", { name });
+    const response: AxiosResponse<StoreType> = await axios.post("/api/store", {
+      name,
+    });
+
     toast.success("Store created", {
       id: loadingToastId,
     });
 
     return response.data;
   } catch (error) {
-    const responseError = error as AxiosError;
+    const responseError = error as AxiosError<FormErrorListType>;
 
     if (responseError.response?.status === 400) {
       toast.error("Store name already exists", {
