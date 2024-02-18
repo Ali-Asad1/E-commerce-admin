d0c549dae9d9c83a3eb8ec752abfc0bbb2de26d7
Ali Asad <aliasad.3128@gmail.com>
2024-02-18 17:37:58 +0330

    feat: Integrate server-side logic to retrieve user's store information

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,25 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "@/lib/prismadb";
+
+async function getCurrentUserStore() {
+  "use server";
+  const { userId } = auth();
+  if (!userId) redirect("/sign-in");
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      userId: userId as string,
+    },
+  });
+
+  return store;
+}
+
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const store = await getCurrentUserStore();
+  if (store) redirect(`/${store.id}`);
+
+  return <>{children}</>;
 }
