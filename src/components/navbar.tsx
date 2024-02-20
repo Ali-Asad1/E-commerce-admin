38332c006ba0099edc50f1308ea68ea7a0f3a457
Ali Asad <aliasad.3128@gmail.com>
2024-02-20 23:19:39 +0330

    feat: Add user profile button to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,12 @@
-const Navbar = () => {
+import { UserButton } from "@clerk/nextjs";
+
+const Navbar = async () => {
 
   return (
     <div className="border-b">
       <div className="h-16 flex items-center px-4">
         <div className="ml-auto">
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </div>
