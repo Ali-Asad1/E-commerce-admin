cc6455e04a2e3fa69a606ec66da57ce8499c0ef0
Ali Asad <aliasad.3128@gmail.com>
2024-02-20 23:19:07 +0330

    feat: Implement dashboard navigation bar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+
+  return (
+    <div className="border-b">
+      <div className="h-16 flex items-center px-4">
+        <div className="ml-auto">
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
