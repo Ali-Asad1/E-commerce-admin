08cafc6217f38f4edf8a6aecdfdc26e182cefb12
Ali Asad <aliasad.3128@gmail.com>
2024-02-24 13:59:21 +0330

    feat: Update Layout component structure

--- src/app/(dashboard)/[storeId]/layout.tsx
+++ src/app/(dashboard)/[storeId]/layout.tsx
@@ -4,7 +4,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
-      {children}
+      <div className="flex flex-col">
+        <div className="flex-1 space-y-4 p-8 pt-6">{children}</div>
+      </div>
     </>
   );
 };
