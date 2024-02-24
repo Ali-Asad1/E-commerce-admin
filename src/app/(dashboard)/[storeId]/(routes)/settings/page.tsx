f21eddac8d64840fbc811cc1143b6a0230a21068
Ali Asad <aliasad.3128@gmail.com>
2024-02-25 02:11:39 +0330

    feat: Refactor settings page for data fetching

--- src/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,9 +1,36 @@
+import { auth } from "@clerk/nextjs";
+import { notFound, redirect } from "next/navigation";
+
+import prismadb from "@/lib/prismadb";
+
 import SettingsForm from "./_components/settingsForm";
 
-const Page = () => {
+async function getCurrentStore(storeId: string) {
+  const user = auth();
+  if (!user) redirect("/sign-in");
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: storeId,
+      userId: user.userId as string,
+    },
+    select: {
+      id: true,
+      name: true,
+    },
+  });
+
+  if (!store) notFound();
+
+  return store;
+}
+
+const Page = async ({ params }: { params: { storeId: string } }) => {
+  const store = await getCurrentStore(params.storeId);
+
   return (
     <>
-      <SettingsForm initialData={{ name: "test", id: "test" }} />
+      <SettingsForm initialData={store} />
     </>
   );
 };
