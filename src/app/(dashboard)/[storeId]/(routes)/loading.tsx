0dab6da9794cffc78162605580cc31944ef1719a
Ali Asad <aliasad.3128@gmail.com>
2024-02-25 22:33:11 +0330

    refactor: Simplify Layout component structure

--- src/app/(dashboard)/[storeId]/(routes)/loading.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/loading.tsx
@@ -4,7 +4,7 @@ import Loader from "@/components/ui/Loader";
 
 export default function Loading() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="w-full h-[calc(100vh-121px)] flex items-center justify-center">
       <Loader />
     </div>
   );
--- src/app/(dashboard)/[storeId]/layout.tsx
+++ src/app/(dashboard)/[storeId]/layout.tsx
@@ -4,9 +4,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col">
-        <div className="flex-1 space-y-4 p-8 pt-6">{children}</div>
-      </div>
+      <div className="space-y-4 p-8 pt-6">{children}</div>
     </>
   );
 };
