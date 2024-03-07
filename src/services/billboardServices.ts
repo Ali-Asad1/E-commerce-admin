65beec258dd075334d5781d44cdc76786f6bf979
Ali Asad <aliasad.3128@gmail.com>
2024-03-07 16:31:56 +0330

    feat: Implement `createBillboard` function for creating billboards

---
+++ src/services/billboardServices.ts
@@ -0,0 +1,35 @@
+import { Billboard } from "@prisma/client";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import toast from "react-hot-toast";
+
+export const createBillboard = async (
+  storeId: string,
+  label: string,
+  imageUrl: string,
+): Promise<Billboard | AxiosError<any>> => {
+  const loadingToastId = toast.loading("Loading...");
+  try {
+    const response: AxiosResponse<Billboard> = await axios.post(`/api/store/${storeId}/billboard`, {
+      label,
+      imageUrl,
+    });
+
+    toast.success("Billboard created", {
+      id: loadingToastId,
+    });
+
+    return response.data;
+  } catch (error: any) {
+    if (error instanceof AxiosError) {
+      if (error.status === 400) {
+        toast.error(error.response?.data, {
+          id: loadingToastId,
+        });
+      }
+    }
+    toast.error("Something went wrong", {
+      id: loadingToastId,
+    });
+    return error;
+  }
+};
