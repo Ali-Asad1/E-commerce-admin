8e1cd99c62f6c63365d65e528f553541c9023640
Ali Asad <aliasad.3128@gmail.com>
2024-03-08 21:40:45 +0330

    feat: Implement `editBillboard` function for updating billboards

--- src/services/billboardServices.ts
+++ src/services/billboardServices.ts
@@ -33,3 +33,36 @@ export const createBillboard = async (
     return error;
   }
 };
+
+export const editBillboard = async (
+  storeId: string,
+  billboardId: string,
+  label: string,
+  imageUrl: string,
+): Promise<Billboard | AxiosError<any>> => {
+  const loadingToastId = toast.loading("Loading...");
+  try {
+    const response: AxiosResponse<Billboard> = await axios.patch(`/api/store/${storeId}/billboard/${billboardId}`, {
+      label,
+      imageUrl,
+    });
+
+    toast.success("Billboard edited", {
+      id: loadingToastId,
+    });
+
+    return response.data;
+  } catch (error: any) {
+    if (error instanceof AxiosError) {
+      if (error.status === 400) {
+        toast.error(error.response?.data, {
+          id: loadingToastId,
+        });
+      }
+    }
+    toast.error("Something went wrong", {
+      id: loadingToastId,
+    });
+    return error;
+  }
+};
