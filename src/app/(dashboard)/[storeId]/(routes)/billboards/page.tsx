151a2a0dcfb112bd6353895c99f24dabcbcb7463
Ali Asad <aliasad.3128@gmail.com>
2024-03-17 01:50:08 +0330

    feat: Fetch and display billboards in Page component

--- src/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,6 +1,39 @@
-import BillboardClient from "./_components/client";
+import { auth } from "@clerk/nextjs";
+import { format } from "date-fns";
+import { notFound, redirect } from "next/navigation";
 
-const Page = () => {
-  return <BillboardClient />;
+import prismadb from "@/lib/prismadb";
+
+import BillboardClient from "./_components/client";
+import { BillboardColumn } from "./_components/columns";
+
+async function getBillboards(storeId: string) {
+  const { userId } = auth();
+  if (!userId) redirect("/sign-in");
+
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  if (!billboards) notFound();
+
+  return billboards;
+}
+
+const Page = async ({ params }: { params: { storeId: string } }) => {
+  const billboards = await getBillboards(params.storeId);
+
+  const formattedBillboards: BillboardColumn[] = billboards.map((billboard) => ({
+    id: billboard.id,
+    label: billboard.label,
+    createAt: format(billboard.createdAt, "d MMM yyy"),
+  }));
+
+  return <BillboardClient data={formattedBillboards} />;
 };
 export default Page;
